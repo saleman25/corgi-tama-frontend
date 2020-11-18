1c00d1667d2b52b253fb2bb19a8128c9dddbdcc9
Samantha Aleman <samanthaaleman@samanthas-air.home>
2020-11-18 13:58:14 -0800

    changed how i wanted login to appear and addd submit/adopt forms/functions

--- src/index.js
+++ src/index.js
@@ -6,26 +6,100 @@
 
 const baseURL = "http://localhost:8080/"
 const mainSection = document.getElementById('main')
-const loginForm = document.getElementById("login-form");
-const loginButton = document.getElementById("login-form-submit");
-const loginErrorMsg = document.getElementById("login-error-msg");
+// const loginForm = document.getElementById("login-form");
+// const loginButton = document.getElementById("login-form-submit");
+// const loginErrorMsg = document.getElementById("login-error-msg");
+const adoptionButton = document.getElementById('adoption-button')
 let state = {page: 'login' }
 
-loginButton.addEventListener("click", (e) => {
-   e.preventDefault();
-    const username = loginForm.username.value 
-    const password = loginForm.password.value 
+// loginButton.addEventListener("click", (e) => {
+//    e.preventDefault();
+//     const username = loginForm.username.value 
+//     const password = loginForm.password.value 
 
-    if (username === "" && password === "") {
-        // blah blah blah 
-    } else {
-        loginErrorMsg.style.opacity = 1;
+//     if (username === "" && password === "") {
+//         // blah blah blah 
+//     } else {
+//         loginErrorMsg.style.opacity = 1;
+//     }
+
+// })
+
+const adopForm = (`
+<h2 id="adopt-header">Adopt Me!</h2>
+        
+<br>
+    
+    <div>
+    <form id="login-form">
+    <input type="text" name="name" id="corgi-name-input placeholder="Name Me!">
+    <div class="field submit">
+    <p class="control" id="adopt-button-area">
+    <button id="adopt-button">
+    ♡ Adopt Me ♡ 
+    </button>
+    </p>
+    </div>
+  
+    </form>
+  
+    <p class="link"><a id="sign-up-link" href="#">Need to Sign Up?</a></p>
+</div>
+`)
+
+const loginForm = (`
+    <h2 id="login-header">Login</h2>
+        
+    <br>
+        
+        <div>
+        <form id="login-form">
+        <input type="text" name="username" id="username-field" class="login-form-field" placeholder="Username">
+        <input type="password" name="password" id="password-field" class="login-form-field" placeholder="Password">
+        
+        <input type="submit" value="Login" id="login-form-submit">
+      
+        </form>
+      
+        <p class="link"><a id="sign-up-link" href="#">Need to Sign Up?</a></p>
+    </div>
+    `
+)
+
+const signupForm = (`
+<h2 id="signup-header">Sign Up</h2>
+        
+    <br>
+        
+        <div>
+        <form id="signup-form">
+        <input type="text" name="username" id="username-field" class="login-form-field" placeholder="Username">
+        <input type="password" name="password" id="password-field" class="login-form-field" placeholder="Password">
+        
+        <input type="submit" value="Sign Up" id="login-form-submit">
+      
+        </form>
+      
+    </div>
+`)
+
+function pageState(){
+    let adoptionButtonDiv = document.getElementById('adoption-button-container')
+    adoptionButtonDiv.hidden = true 
+
+    switch(state.page){
+        case('login'):
+        mainSection.innerHTML = loginForm 
+        break;
+        case('signup'):
+        mainSection.innerHTML = signupForm 
+        break;
     }
+}
 
-})
+let submit = document.querySelector(".submit")
+let linkArea = document.querySelector(".link")
+linkArea.addEventListener('click', renderLoginOrSignup)
+submit.addEventListener('click', loginOrSignup)
 
-// yr gonna have to prob make a const for loging and signup so u can render it depending on what is chosen
-// render the page depending on what it chose
-    // so login signup 
-// u need to do a signuup and login preventdefaults and keep logged in and 
-// then form so u can adopt a corgi
+function loginOrSignup(e)
