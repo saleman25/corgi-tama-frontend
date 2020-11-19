60a047ffd37540f30c251b62ff17182b0580adb9
Samantha Aleman <samanthaaleman@samanthas-air.home>
2020-11-18 16:11:54 -0800

    added more to my login/signup function

--- src/index.js
+++ src/index.js
@@ -1,29 +1,14 @@
-//adopt form
-//login form
-//signup form
 //alerts
 //this :3 is :3 alot :3 
 
 const baseURL = "http://localhost:8080/"
+let currentUser
+const userAdapter = new UserAdapter(BASEURL)
+const corgiAdapter = new CorgiAdapter(BASEURL+'/corgis')
 const mainSection = document.getElementById('main')
-// const loginForm = document.getElementById("login-form");
-// const loginButton = document.getElementById("login-form-submit");
-// const loginErrorMsg = document.getElementById("login-error-msg");
 const adoptionButton = document.getElementById('adoption-button')
 let state = {page: 'login' }
 
-// loginButton.addEventListener("click", (e) => {
-//    e.preventDefault();
-//     const username = loginForm.username.value 
-//     const password = loginForm.password.value 
-
-//     if (username === "" && password === "") {
-//         // blah blah blah 
-//     } else {
-//         loginErrorMsg.style.opacity = 1;
-//     }
-
-// })
 
 const adopForm = (`
 <h2 id="adopt-header">Adopt Me!</h2>
@@ -43,7 +28,6 @@ const adopForm = (`
   
     </form>
   
-    <p class="link"><a id="sign-up-link" href="#">Need to Sign Up?</a></p>
 </div>
 `)
 
@@ -54,8 +38,8 @@ const loginForm = (`
         
         <div>
         <form id="login-form">
-        <input type="text" name="username" id="username-field" class="login-form-field" placeholder="Username">
-        <input type="password" name="password" id="password-field" class="login-form-field" placeholder="Password">
+        <input type="text" name="username" id="login-username-field" class="login-form-field" placeholder="Username">
+        <input type="password" name="password" id="login-password-field" class="login-form-field" placeholder="Password">
         
         <input type="submit" value="Login" id="login-form-submit">
       
@@ -73,13 +57,13 @@ const signupForm = (`
         
         <div>
         <form id="signup-form">
-        <input type="text" name="username" id="username-field" class="login-form-field" placeholder="Username">
-        <input type="password" name="password" id="password-field" class="login-form-field" placeholder="Password">
+        <input type="text" name="username" id="signup-username-field" class="login-form-field" placeholder="Username">
+        <input type="password" name="password" id="signup-password-field" class="login-form-field" placeholder="Password">
         
-        <input type="submit" value="Sign Up" id="login-form-submit">
+        <input type="submit" value="Sign Up" id="singup-form-submit">
       
         </form>
-      
+        <p class="link" ><a id="log-in-link" href="#">Or Log In Here</a></p>
     </div>
 `)
 
@@ -97,9 +81,47 @@ function pageState(){
     }
 }
 
-let submit = document.querySelector(".submit")
-let linkArea = document.querySelector(".link")
-linkArea.addEventListener('click', renderLoginOrSignup)
-submit.addEventListener('click', loginOrSignup)
 
-function loginOrSignup(e)
+function loginOrSignup(e){
+    e.preventDefault()
+    if (e.target.id === "singup-form-submit"){
+        
+        let userObj = { user: { 
+        username: document.getElementById("signup-username-field").value, 
+        password: document.getElementById("signup-password-field").value
+        }
+        }
+        userAdapter.signUpUser(userObj)
+    } else if(e.target.id === "login-button-submit"){
+        let userObj = { user: {
+            username: document.getElementById("login-username-field").value, 
+            password: document.getElementById("login-password-field").value
+        }
+    }
+        userAdapter.loginUser(userObj)
+    }
+    document.querySelector('form').reset()
+}
+
+function renderLoginOrSignup(e){
+    e.preventDefault()
+    if (e.target.id === "log-in-link"){
+        state.page = "login"
+        pageState()
+    } else if (e.target.id === "sign-up-link")
+    state.page = "signup"
+    pageState()
+}
+
+function renderAdoption(){
+    mainSection.innerHTML = adopForm
+    let adoptButton = document.getElementById('adopt-button')
+    adoptButton.addEventListener('click', adopt)
+}
+
+function adopt(e){
+    e.taget.disabled = true 
+    e.preventDefault()
+    let name = document.getElementById("corgi-name-input").value
+    corgiAdapter.createCorgi(name)
+}
