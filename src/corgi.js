16cb2e006fa6ef93ab077cdaad66740e22344213
Samantha Aleman <samantha.aleman25@gmail.com>
2020-11-18 22:49:20 -0800

    started coding the elemets of the game

--- src/corgi.js
+++ src/corgi.js
@@ -8,7 +8,7 @@ class Corgi {
         this.happiness_meter = happiness_meter;
         this.evolution_countdown = evolution_countdown;
         this.corgi_type = corgi_type;
-        //this.gif = gifs ima use
+        // this.gif = code for the corgi gif ima use
 
         Corgi.all.push(this)
     }
@@ -21,3 +21,39 @@ class Corgi {
 //gametimer
 //pls someonesaysike
 // how it grows up
+
+setGif(){
+    // gif i am going to use
+}
+
+renderProgressBarsInnerHTML(){
+    // in here is going to go allthe corgi factors
+}
+
+renderCorgi(){
+let corgiDiv = document.createElement('div')
+this.div = corgiDiv
+corgiDiv.className =  "corgi-div"
+corgiDiv.dataset.id = `${this.id}`
+
+//create element for spite img
+let babypup = document.createElement('img')
+this.babypup = babypup
+babypup.src = this.setGif()
+
+corgiDiv.appendChild(babypup)
+
+let progressBarsContainer = document.createElement('div')
+progressBarsContainer.className = "progress-bars has-text-centered"
+progressBarsContainer.id = `${this.id}-progress-bars`
+
+
+this.progressBarsContainer = progressBarsContainer
+progressBarsContainer.innerHTML = this.renderProgressBarsInnerHTML()
+
+//create div to hold all of the buttons
+let buttonsArea = document.createElement('div')
+buttonsArea.className = "buttons-area"
+
+}
+
